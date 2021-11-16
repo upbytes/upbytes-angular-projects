55fbb3834b3318808edb5d5ddcc1484ed5a46a5d
vaibhav <vaibhav@upbytes.in>
2021-11-16 18:01:22 +0530

    ACG-3 move calendar demo code into this repo

--- projects/upbytes-angular-calendar/src/app/app.module.ts
+++ projects/upbytes-angular-calendar/src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularMaterialCalendarModule } from '@upbytes.in/angular-material-calendar';
-
+import { FontAwesomeModule, FaIconLibrary, FaConfig} from '@fortawesome/angular-fontawesome';
+import { faStackOverflow, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarDemoHeader } from './header/calendar-demo-header.component';
+import { MaterialModule } from './material-module/material.module';
+import { CalendarDemoSideNav } from './sidenav/calendar-demo-sidenav.component';
+import { CalendarDemoEvents } from './calendar-demo-events/calendar-demo-events.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarDemoHeader,
+    CalendarDemoSideNav,
+    CalendarDemoEvents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
     AngularMaterialCalendarModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary, faconfig: FaConfig) {
+    library.addIcons( faStackOverflow, faGithub, faMedium);
+    faconfig.defaultPrefix = 'fab';
+
+  }
+}
---
+++ projects/upbytes-angular-calendar/src/app/header/calendar-demo-header.component.ts
@@ -0,0 +1,36 @@
+import { Component, Renderer2 } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { THEMES } from '../data/calendar-theme-data';
+import { CalendarTheme } from '../model/calendar-theme';
+import { MatIconRegistry } from '@angular/material/icon';
+
+@Component({
+    selector: 'calendar-demo-header',
+    templateUrl: './calendar-demo-header.component.html',
+    styleUrls: ['./calendar-demo-header.component.scss']
+})
+export class CalendarDemoHeader {
+    themes: CalendarTheme[] = THEMES;
+
+    constructor(
+        private _renderer: Renderer2,
+        private matIconRegistry: MatIconRegistry,
+        private domSanitizer: DomSanitizer) {
+        this.matIconRegistry.addSvgIcon(
+            `upbytes-calendar-icon`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/upbytes-calendar-icon.svg')
+          );
+    }
+
+    changeTheme(option: CalendarTheme) {
+        this.clearTheme();
+        option.isChecked = !option.isChecked;
+        this._renderer.addClass(document.body, option.class!);
+    }
+
+    clearTheme() {
+        this.themes.map(o => o.isChecked = false);
+        this.themes.forEach(o => this._renderer.removeClass(document.body, o.class!));
+    }
+
+}
