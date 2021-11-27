5a13f00b249cc90779c179a1393b62bc8bcc2422
vaibhav <vaibhav@upbytes.in>
2021-11-27 18:45:09 +0530

    ACG-15 apply code css in guide html

--- build-component.js
+++ build-component.js
@@ -5,7 +5,8 @@ build = async () =>{
     const files = [
         './dist/upbytes-calendar-guide/runtime.js',
         './dist/upbytes-calendar-guide/polyfills.js',
-        './dist/upbytes-calendar-guide/main.js'
+        './dist/upbytes-calendar-guide/main.js',
+        './dist/upbytes-calendar-guide/styles.css'
       ];
     
       await fs.ensureDir('projects/upbytes-angular-calendar/src/assets/widget');
--- projects/upbytes-calendar-guide/src/app/doc-viewer/doc-viewer.component.ts
+++ projects/upbytes-calendar-guide/src/app/doc-viewer/doc-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { DocumentViewerService } from '../services/document-viewer.service';
 
@@ -6,7 +6,50 @@ import { DocumentViewerService } from '../services/document-viewer.service';
 @Component({
   selector: 'upbytes-doc-viewer',
   templateUrl: './doc-viewer.component.html',
-  styleUrls: ['./doc-viewer.component.scss']
+  styles: [`
+  .doc-viewer-container {
+    margin: 50px;
+  }
+  .doc-code-container {
+    border-radius: 4px;
+    border: 1px solid rgba(0,0,0,.2);
+    background-color: #282a36;
+    color: #4a97e8;
+    font-family: 'Droid Sans Mono', 'monospace', monospace, 'Droid Sans Fallback';font-weight:normal;
+    font-size: 14px;
+    line-height: 19px;
+  }
+  .doc-hljs-keyword {
+    color: #e37b40;
+  }
+
+  .doc-hljs-string {
+    color: #f0be37;
+  }
+
+  .doc-hljs-variable-name {
+    color: #ffb86c;
+    font-style: italic;
+  }
+  
+  .doc-hljs-class-name {
+    color: #00ffff;
+  }
+  .doc-hljs-fn-name {
+    color:#46b29d;
+  }
+
+  .doc-hljs-import-class {
+    color:#46b29d;
+    font-style: italic;
+  }
+
+  .doc-hljs-fn-paranthesis {
+    color:#ffffff;
+  }
+  
+  `],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DocViwerComponent {
   title = 'upbytes-calendar-guide';
@@ -14,8 +57,8 @@ export class DocViwerComponent {
   constructor(
     private _domSanitizer: DomSanitizer,
     private documentViewerService: DocumentViewerService) {
-      this.documentViewerService.getGuideData().subscribe((doc) => {
-        this.docHtml = this._domSanitizer.bypassSecurityTrustHtml(doc);
-      });
+    this.documentViewerService.getGuideData().subscribe((doc) => {
+      this.docHtml = this._domSanitizer.bypassSecurityTrustHtml(doc);
+    });
   }
 }
